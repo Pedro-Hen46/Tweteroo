b932c20fdc2048bc4fbd7c2eab4a368141864a93
Pedro Henrique <driverique@gmail.com>
2022-06-19 09:21:11 -0300

    Fazendo login e aparecendo os tweets

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
+let users = [];
+let tweets = [];
 
-app.get("/", (request, response) => {
+app.post("/sign-up", (request, response) => {
+  const user = request.body;
+  users.push(user);
+  response.status(201).send("OK");
+});
 
-    response.send("Você está na pasta raiz do programa.");
-})
+app.get("/tweets", (request, response) => {
+  const tweet = request.body;
+  response.send(tweets);
+});
 
-
-
+app.post("/tweets", (request, response) => {
+  const tweet = request.body;
+  tweets.push(tweet);
+  response.status(201).send("OK");
+});
 
 
 app.listen(5000, () => {
-    console.log("Servidor está rodando na porta 5000");
+  console.log("Servidor está rodando na porta 5000");
 });
